d24081af275d5cfea64652d7d0a568653f1f1ade
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2024-03-06 06:18:29 +0300

    Bölüm 1

---
+++ API/Data/VillaStore.cs
@@ -0,0 +1,18 @@
+﻿using API.Models.Dtos;
+
+namespace API.Data
+{
+    public static class VillaStore
+    {
+
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1, Name = "Pool View", Sqft = 100, Occupancy = 4 },
+            new VillaDTO { Id = 2, Name = "Beach", Sqft = 300, Occupancy =  3}
+        };
+
+
+
+
+    }
+}
