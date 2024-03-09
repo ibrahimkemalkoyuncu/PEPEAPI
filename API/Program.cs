c3541b252e6d08c3510092e0e0772a7c42b7cee9
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2024-03-09 11:05:43 +0300

    Bölüm 3 kadar tamamlandı.

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers(option=>
+{
+    option.ReturnHttpNotAcceptable = true;  
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
