698ccd7f7e91594f5e9f1d3034f6158002c2ab02
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2024-03-14 00:10:51 +0300

    Bölüm 6: Repository Tanımı eklendi

---
+++ API/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,17 @@
+﻿using API.Models;
+using System.Linq.Expressions;
+
+namespace API.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa,bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa,bool>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);  
+
+        Task Save();
+
+
+    }
+}
